187a7784538df4e120dee1e25dccb43ed6cf5723
eduardoesnc <eduardoestevaonc@gmail.com>
2023-02-15 16:03:42 -0300

    Apresentação de mais tratamentos

--- pages/02_Tratamento_dos_dados.py
+++ pages/02_Tratamento_dos_dados.py
@@ -34,10 +34,7 @@ def tratarDados(database):
     # Apagar coluna policy_id, já que são apenas IDs
     database = database.drop(['policy_id'], axis=1)
 
-    # Voltando idade do segurado para o normal
-    # df['age_of_policyholder'] = round(df['age_of_policyholder'].mul(100))
-
-    # Separando as tabelas max_torque e max_power
+    # Consertando as colunas max_torque e max_power
     database["max_torque_Nm"] = database['max_torque'].\
         str.extract(r"([-+]?[0-9]*\.?[0-9]+)(?=\s*Nm)").astype('float64')
     database["max_torque_rpm"] = database['max_torque'].\
@@ -49,9 +46,9 @@ def tratarDados(database):
     database = database.drop(['max_torque'], axis=1)
     database = database.drop(['max_power'], axis=1)
 
-    # Tranformar as colunas largura tamanho e altura em apenas uma coluna chamada volume
-    database['volume'] = np.log(database.length * database.width * database.height * 1e-6)
-    database = database.drop(['length', 'width', 'height'], axis=1)
+    # # Tranformar as colunas largura tamanho e altura em apenas uma coluna chamada volume
+    # database['volume'] = np.log(database.length * database.width * database.height * 1e-6)
+    # database = database.drop(['length', 'width', 'height'], axis=1)
 
     # Normalizar policy tenure com min max normalization
     # policy_df = bf['policy_tenure']
@@ -60,6 +57,38 @@ def tratarDados(database):
 
     return database
 
+
+# Criação de array com o nome de todas as colunas para facilitar na criação dos filtros
+dict_nome_colunas = ['Idade do carro em anos', 'Idade do segurado em anos', 'Área do segurado',
+                     'Densidade populacional',
+                     'Código da fabricante do carro', 'Segmento do carro (A / B1 / B2 / C1 / C2)', 'Modelo do carro',
+                     'Tipo de combustível usado no carro', 'Torque máximo gerado pelo carro',
+                     'Força máxima gerada pelo carro',
+                     'Tipo de motor usado pelo carro', 'Número de airbags instalados no carro',
+                     'Tem controle de estabilização eletrônica?',
+                     'O volante é ajustável?', 'Tem sistema de monitoramento da pressão do pneu?',
+                     'Tem sensores de ré?',
+                     'Tem câmera de ré?', 'Tipo de freio usado no carro', 'Cilindradas do motor (cc)',
+                     'Quantidade de cilindros do carro',
+                     'Tipo de transmissão do carro', 'Quantidade de marchas do carro', 'Tipo de direção do carro',
+                     'Espaço necessário pro carro fazer uma certa curva',
+                     'Volume do carro', 'Peso máximo suportado pelo carro',
+                     'Tem farol de neblina?', 'Tem limpador de vidro traseiro?', 'Tem desembaçador de vidro traseiro?',
+                     'Tem assistência de freio?',
+                     'Tem trava elétrica de porta?', 'Tem direção hidráulica?', 'O acento do motorista é ajustável?',
+                     'Tem espelho de retrovisor traseiro?',
+                     'Tem luz indicativa de problemas no motor?', 'Tem sistema de alerta de velocidade?',
+                     'Classificação de segurança pela NCAP (de 0 a 5)']
+nome_colunas = ['age_of_car', 'age_of_policyholder', 'area_cluster', 'population_density', 'make', 'segment',
+                'model', 'fuel_type', 'max_torque_Nm', 'max_power_bhp', 'engine_type', 'airbags', 'is_esc',
+                'is_adjustable_steering', 'is_tpms',
+                'is_parking_sensors', 'is_parking_camera', 'rear_brakes_type', 'displacement', 'cylinder',
+                'transmission_type', 'gear_box', 'steering_type',
+                'turning_radius', 'volume', 'gross_weight', 'is_front_fog_lights', 'is_rear_window_wiper',
+                'is_rear_window_washer',
+                'is_rear_window_defogger', 'is_brake_assist', 'is_power_door_locks', 'is_central_locking',
+                'is_power_steering', 'is_driver_seat_height_adjustable', 'is_day_night_rear_view_mirror',
+                'is_ecw', 'is_speed_alert', 'ncap_rating', 'is_claim']
 
 bf = tratarDados(bf)
 numericos = bf.select_dtypes(include=[np.float64, np.int64])
@@ -73,21 +102,79 @@ st.markdown(
     """, unsafe_allow_html=True
 )
 st.markdown("---")
+st.markdown("<h2>Normalização do Tempo de seguro</h2>", unsafe_allow_html=True)
+st.markdown("""<p style="font-size: 16px;text-align: center; margin-top: 0px">
+            A coluna que representa o Tempo de seguro deve ser normalizada para manter-los em uma escala comum e para
+             trabalharmos melhor com o algoritmo de Machine Learning.
+            </p>""", unsafe_allow_html=True)
+A1, A2 = st.columns(2)
+with A1:
+    st.markdown("<h5 style='text-align: center;margin-bottom: 0px;'>Original</h5>", unsafe_allow_html=True)
+    columnsPT = bf['policy_tenure']
+    st.dataframe(columnsPT)
+
+with A2:
+    st.markdown("<h5 style='text-align: center;margin-bottom: 0px;'>Normalizada</h5>", unsafe_allow_html=True)
+    policy_df = bf['policy_tenure']
+    normPolicy = (policy_df - policy_df.min()) / (policy_df.max() - policy_df.min())
+    normPolicy = pd.concat([normPolicy], axis=1)
+    st.dataframe(normPolicy)
+
+st.markdown("---")
+
+st.markdown("<h2>Altura, largura e comprimento para volume</h2>", unsafe_allow_html=True)
+st.markdown("""<p style="font-size: 16px;text-align: center; margin-top: 0px">
+            As colunas altura, largura e comprimento podem ser simplificadas em uma única coluna chamada Volume.
+            </p>""", unsafe_allow_html=True)
+
+B1, B2, B3 = st.columns(3)
+with B1:
+    st.markdown("<h5 style='text-align: center;margin-bottom: 0px;'>Original</h5>", unsafe_allow_html=True)
+    columnsALC = pd.concat([bf['length'], bf['width'], bf['height']], axis=1)
+    st.dataframe(columnsALC)
+
+with B2:
+    # Transformando em apenas uma coluna
+    st.markdown("<h5 style='text-align: center;margin-bottom: 0px;'>Transformada</h5>", unsafe_allow_html=True)
+    # Os valores estavam em milímetros
+    bf['volume'] = np.log(bf.length * bf.width * bf.height * 1e-6)
+    bf = bf.drop(['length', 'width', 'height'], axis=1)
+    columnVol = bf['volume']
+    st.dataframe(columnVol)
+
+with B3:
+    # Padronizandos os valores
+    st.markdown("<h5 style='text-align: center;margin-bottom: 0px;'>Normalizada</h5>", unsafe_allow_html=True)
+    normColumnVol = columnVol
+    normColumnVol = (normColumnVol - normColumnVol.min()) / (normColumnVol.max() - normColumnVol.min())
+    normColumnVol = pd.concat([normColumnVol], axis=1)
+    st.dataframe(normColumnVol)
+
+st.markdown("---")
 
 st.markdown("<h2>Outliers</h2>", unsafe_allow_html=True)
 st.markdown("""<p style="font-size: 14px;font-style: italic;color: gray; margin-top: 
     -15px">Outliers são valores atípicos ou extremos em uma série de dados que podem afetar a análise e precisam ser 
     identificados e tratados antes da análise estatística.</p>""", unsafe_allow_html=True)
 
-st.markdown("<br> <h4 style='text-align: center;'>Idade do carro</h4>", unsafe_allow_html=True)
-A1, A2 = st.columns(2)
+# Filtragens do gráfico usado apenas para a busca de Outliers
+# st.markdown("<br> <h4 style='text-align: center;'>Busca por Outliers</h4>", unsafe_allow_html=True)
+# option = st.selectbox(
+#     'Seleciona um para comparar com a possibilidade de reivindicação dentro de 6 meses:',
+#     dict_nome_colunas)
+# option = dict_nome_colunas.index(option)
+# fig = px.box(bf, x=nome_colunas[option], labels={nome_colunas[option]: dict_nome_colunas[option]}, height=300)
+# st.write(fig)
 
-with A1:
+st.markdown("<br> <h4 style='text-align: center;'>Idade do carro</h4>", unsafe_allow_html=True)
+C1, C2 = st.columns(2)
+
+with C1:
     st.markdown("<h5 style='text-align: center;margin-bottom: -34px;'>Antes</h5>", unsafe_allow_html=True)
     fig = px.box(bf, x='age_of_car', labels={'age_of_car': 'Idade do carro'}, width=400, height=300)
     st.write(fig)
 
-with A2:
+with C2:
     st.markdown("<h5 style='text-align: center; margin-bottom: -34px;'>Depois</h5>", unsafe_allow_html=True)
     age_of_car_outliers = bf.age_of_car > bf.age_of_car.quantile(0.995)
     bf = bf.loc[~age_of_car_outliers]
@@ -95,28 +182,25 @@ with A2:
     fig = px.box(bf, x='age_of_car', labels={'age_of_car': 'Idade do carro'}, width=400, height=300)
     st.write(fig)
 
-st.markdown("""<p style="font-size: 16px;text-align: center; margin-top: -20px">
-            A coluna da idade do carro apresentava alguns valores muito diferentes do comum, sendo assim foram removidas
-            algumas linhas que apresentavam esses valores extremos.
-            </p>""", unsafe_allow_html=True)
+st.markdown("<h4 style='text-align: center;'>Idade do segurado</h4>", unsafe_allow_html=True)
+D1, D2 = st.columns(2)
 
-st.markdown("<br> <h4 style='text-align: center;'>Idade do segurado</h4>", unsafe_allow_html=True)
-B1, B2 = st.columns(2)
-
-with B1:
+with D1:
     st.markdown("<h5 style='text-align: center;margin-bottom: -34px;'>Antes</h5>", unsafe_allow_html=True)
-    fig = px.box(bf, x='age_of_policyholder', labels={'age_of_car': 'Idade do segurado'}, width=400, height=300)
+    fig = px.box(bf, x='age_of_policyholder', labels={'age_of_policyholder': 'Idade do segurado'},
+                 width=400, height=300)
     st.write(fig)
 
-with B2:
+with D2:
     st.markdown("<h5 style='text-align: center; margin-bottom: -34px;'>Depois</h5>", unsafe_allow_html=True)
     age_of_car_outliers = bf.age_of_policyholder > bf.age_of_policyholder.quantile(0.995)
     bf = bf.loc[~age_of_car_outliers]
 
-    fig = px.box(bf, x='age_of_policyholder', labels={'age_of_car': 'Idade do segurado'}, width=400, height=300)
+    fig = px.box(bf, x='age_of_policyholder', labels={'age_of_policyholder': 'Idade do segurado'},
+                 width=400, height=300)
     st.write(fig)
 
 st.markdown("""<p style="font-size: 16px;text-align: center; margin-top: -20px">
-            A coluna da idade do segurado apresentava alguns valores muito diferentes do comum, sendo assim foram removidas
-            algumas linhas que apresentavam esses valores extremos.
+            As colunas da idade do carro e da idade do segurado apontavam alguns valores muito diferentes do comum, 
+            sendo assim foram removidas algumas linhas que apresentavam esses valores extremos.
             </p>""", unsafe_allow_html=True)
